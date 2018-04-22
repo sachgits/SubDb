d42fc9750bf3de7eb1d70d70577b4c70f094a73e
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-04-22 18:32:59 -0400

    initial

---
+++ SubDb/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MediaBrowser.Common.Plugins;
+
+namespace SubDb
+{
+    public class Plugin : BasePlugin
+    {
+        private Guid _id = new Guid("F015EA06-B413-47F1-BF15-F049A799658B");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        public override string Name
+        {
+            get { return StaticName; }
+        }
+
+        public static string StaticName
+        {
+            get { return "SubDb"; }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Download subtitles from Subdb";
+            }
+        }
+    }
+}
